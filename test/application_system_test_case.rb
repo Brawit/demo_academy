c2051a411f916b94582c90f541e1140c91065743
Prawit <prawit@Prawits-MacBook-Air.local>
2025-08-18 20:20:47 +0700

    test: change CI github to headless browser

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,16 @@
 require "test_helper"
+require "capybara/rails"
+require "selenium/webdriver"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ]
+  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ], options: {
+    args: %w[
+      headless
+      disable-gpu
+      no-sandbox
+      disable-dev-shm-usage
+      window-size=1400,1400
+      user-data-dir=/tmp/chrome-user-data
+    ]
+  }
 end
