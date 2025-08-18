e45707e6e68bcb2a4bd1efe8e385a30e6dc4e173
Prawit <prawit@Prawits-MacBook-Air.local>
2025-08-18 20:37:56 +0700

    test: change CI test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,14 +3,13 @@ require "capybara/rails"
 require "selenium/webdriver"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ], options: {
-    args: %w[
-      headless
-      disable-gpu
-      no-sandbox
-      disable-dev-shm-usage
-      window-size=1400,1400
-      user-data-dir=/tmp/chrome-user-data
-    ]
-  }
+  chrome_options = Selenium::WebDriver::Chrome::Options.new
+  chrome_options.add_argument("--headless")
+  chrome_options.add_argument("--disable-gpu")
+  chrome_options.add_argument("--no-sandbox")
+  chrome_options.add_argument("--disable-dev-shm-usage")
+  chrome_options.add_argument("--window-size=1400,1400")
+  chrome_options.add_argument("--user-data-dir=/tmp/chrome-user-data")
+
+  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ], options: chrome_options
 end
