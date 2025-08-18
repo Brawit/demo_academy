fbbe93c87401cf6ba1b93fb2c0f428efe2016bfe
Prawit <prawit@Prawits-MacBook-Air.local>
2025-08-18 20:31:06 +0700

    test: change CI github to headless browser with pre version

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,14 +3,14 @@ require "capybara/rails"
 require "selenium/webdriver"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  # สร้าง Chrome options ใหม่
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument("--headless")
-  options.add_argument("--disable-gpu")
-  options.add_argument("--no-sandbox")
-  options.add_argument("--disable-dev-shm-usage")
-  options.add_argument("--window-size=1400,1400")
-  options.add_argument("--user-data-dir=/tmp/chrome-user-data")
-
-  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ], options: options
+  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ], options: {
+    args: %w[
+      headless
+      disable-gpu
+      no-sandbox
+      disable-dev-shm-usage
+      window-size=1400,1400
+      user-data-dir=/tmp/chrome-user-data
+    ]
+  }
 end
